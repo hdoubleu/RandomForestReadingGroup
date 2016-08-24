dfbba61bfbe6a89a1106b27dd0aebf640c9e153a
stephentwright <st.wright@gmail.com>
2016-08-24 16:06:40 +1000

    upload docs

---
+++ R Scripts/Titanic_RandomForest.R
@@ -0,0 +1,48 @@
+######## SCRIPT DOWNLOADED FROM KAGGLE ########
+# https://www.kaggle.com/benhamner/titanic/random-forest-benchmark-r/code
+
+#Load Libraries
+library(ggplot2)
+library(randomForest)
+
+#Replicate Results
+set.seed(1)
+train <- read.csv("../input/train.csv", stringsAsFactors=FALSE)
+test  <- read.csv("../input/test.csv",  stringsAsFactors=FALSE)
+
+extractFeatures <- function(data) {
+  features <- c("Pclass",
+                "Age",
+                "Sex",
+                "Parch",
+                "SibSp",
+                "Fare",
+                "Embarked")
+  fea <- data[,features]
+  fea$Age[is.na(fea$Age)] <- -1
+  fea$Fare[is.na(fea$Fare)] <- median(fea$Fare, na.rm=TRUE)
+  fea$Embarked[fea$Embarked==""] = "S"
+  fea$Sex      <- as.factor(fea$Sex)
+  fea$Embarked <- as.factor(fea$Embarked)
+  return(fea)
+}
+
+rf <- randomForest(extractFeatures(train), as.factor(train$Survived), ntree=100, importance=TRUE)
+
+submission <- data.frame(PassengerId = test$PassengerId)
+submission$Survived <- predict(rf, extractFeatures(test))
+write.csv(submission, file = "1_random_forest_r_submission.csv", row.names=FALSE)
+
+imp <- importance(rf, type=1)
+featureImportance <- data.frame(Feature=row.names(imp), Importance=imp[,1])
+
+p <- ggplot(featureImportance, aes(x=reorder(Feature, Importance), y=Importance)) +
+  geom_bar(stat="identity", fill="#53cfff") +
+  coord_flip() + 
+  theme_light(base_size=20) +
+  xlab("") +
+  ylab("Importance") + 
+  ggtitle("Random Forest Feature Importance\n") +
+  theme(plot.title=element_text(size=18))
+
+ggsave("2_feature_importance.png", p)
