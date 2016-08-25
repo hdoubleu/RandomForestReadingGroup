7545dbf69d164bc4cc1f509f8abef2fd5d795997
stephentwright <st.wright@gmail.com>
2016-08-25 11:27:49 +1000

    added explore script

---
+++ R Scripts/Titanic_Explore.R
@@ -0,0 +1,7 @@
+##############################################
+#   EXPLORING THE TITANIC DATASET            #
+##############################################
+
+#if interested see this link - very good
+#https://www.kaggle.com/mrisdal/titanic/exploring-survival-on-the-titanic/comments
+
